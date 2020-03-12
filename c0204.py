a7520165af55de8e2d7beb7577a029074b0af48d
tester2802 <forsamapp0912@gmil.com>
2020-03-12 13:16:02 +0600

    after lex

---
+++ c0204.py
@@ -0,0 +1,8 @@
+'''
+4) Minimal positive integer: Find minimal positive integer that is not in list.
+Example: [1,2,3,4,6] - Answer 5
+Example: [1,2,3] - Answer 4
+Example: [-1, -2, -6] - Answer 1
+Create effective algorithm
+'''
+
