96f0a70232e4d84975a43119831a39866bf80511
tester2802 <forsamapp0912@gmil.com>
2020-03-13 12:53:52 +0600

    del

--- pr02.py
+++
@@ -1,2 +0,0 @@
-a = {i**2:i for i in range (8,15)}
-print(a)
--- s01.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Enter the number 5 digit: "))
-a = x//10000
-b = x//1000%10
-c = x//100%10
-d = x//10%10
-e = x%10
-
-print(a,b,c,d,e)
-print(a+b+c+d+e)
